a9b38c49838e5bdca9f93005e97058bc8b4110c4
Valeria Hernández Montesino <valeriahernan@gmail.com>
2026-05-26 23:59:15 -0400

    info talleres

--- script.js
+++ script.js
@@ -25,42 +25,42 @@ const workshops = [
     title: "Taller 01",
     image: "images/t0.png",
     description:
-      "Exploración visual y experimental enfocada en composición, imagen y procesos colectivos."
-  },
+      "Este taller se propone como una iniciación a la producción musical con énfasis en la creación musical digital, destacando la autodeterminación creativa que este proceso permite más allá de los medios técnicos o los conocimientos teóricos. Incentivando una aproximación DIY (do it yourself) a estos recursos, se enseñará el uso de herramientas estándar de producción musical en un software de audio, incentivando la experimentación creativa de éstas, el potencial creativo de las limitaciones y del no-saber-hacer. Con contenidos progresivos y jerarquizados, se guiará el trabajo de las distintas sesiones a la producción de un registro  musical de lxs participantes, que culminará con la recopilación y publicación de estos resultados en un compilado que será distribuido gratuitamente a través de las distintas plataformas digitales de Talleres Raros."  
+    },
 
   {
     title: "Taller 02",
     image: "images/t1.png",
     description:
-      "Instancia abierta de creación visual, referencias gráficas y conversación interdisciplinaria."
+      "Taller enfocado en la creación de letras para canciones, donde se facilitarán herramientas y ejercicios que permitan a lxs talleristas encontrar una voz lírica propia y mecanismos para desarrollar ideas escriturales, vencer el “bloqueo de escritura”, etc. En el taller, además, se entregará material didáctico para trabajar en la forma de pistas de acceso libre y originales, producidas especialmente para que puedan ser usadas por lxs talleristas, en caso de necesitarlas."
   },
 
   {
     title: "Taller 03",
     image: "images/t2.png",
     description:
-      "Workshop dedicado a procesos editoriales, identidad visual y narrativa contemporánea."
+      "Este taller busca indagar en los recursos de composición disponibles en la producción musical desde el computador, posicionándolo como instrumento creativo central. En sesiones en formato de workshops prácticos, se indagarán técnicas de diseño y experimentación sonora, exploración textural y técnicas de música generativa (perspectivas nuevas en torno al samplear, al MIDI, a la aleatoriedad, entre otras)"
   },
 
   {
     title: "Taller 04",
     image: "images/t3.png",
     description:
-      "Laboratorio experimental enfocado en arte digital, diseño y materialidad visual."
+      "Este taller se propone como una iniciación a la creación musical digital para personas trans, planteando perspectivas tanto críticas como prácticas en torno a la grabación y la producción, y buscando dar un espacio para compartir problemáticas, perspectivas y experiencias personales en torno a la producción que son particulares para las identidades disidentes. Se contempla la discusión en torno a la representación del cuerpo, la voz y referentes musicales trans; demostraciones prácticas de cómo armar una primera maqueta en Ableton Live; para finalizar con un taller grupal en donde compartir y comentar los ejercicios creados."
   },
 
   {
     title: "Taller 05",
     image: "images/t4.png",
     description:
-      "Encuentro colaborativo para desarrollar ideas visuales y ejercicios curatoriales."
+      "Masterclass Javiera Electra."
   },
 
   {
     title: "Taller 06",
     image: "images/t5.png",
     description:
-      "Espacio de experimentación artística y producción visual contemporánea."
+      "Masterclass Lilen."
   }
 
 ];
