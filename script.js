e2ad274bef66ffb6d4ae59bfc11b52da4002cab9
Valeria Hernández Montesino <valeriahernan@gmail.com>
2026-05-27 12:43:19 -0400

    ok2

--- script.js
+++ script.js
@@ -2,18 +2,14 @@ const scenes = document.querySelectorAll(".scene");
 const links = document.querySelectorAll(".nav-menu a");
 
 function show(id){
-
   scenes.forEach(s => s.classList.remove("active"));
-
-  const target = document.querySelector(id);
-  if(target) target.classList.add("active");
-
+  document.querySelector(id).classList.add("active");
 }
 
-links.forEach(link => {
-  link.addEventListener("click", e => {
+links.forEach(l=>{
+  l.addEventListener("click", e=>{
     e.preventDefault();
-    show(link.getAttribute("href"));
+    show(l.getAttribute("href"));
   });
 });
 
