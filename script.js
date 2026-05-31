3566a7d6256ce2364e8fc7aff1765eca87deeb33
Valeria Hernández Montesino <valeriahernan@gmail.com>
2026-05-31 16:38:55 -0400

    update

--- script.js
+++ script.js
@@ -2,30 +2,16 @@ const scenes = document.querySelectorAll(".scene");
 const links = document.querySelectorAll(".nav-menu a");
 
 function showScene(id){
-
-  scenes.forEach(scene=>{
-    scene.classList.remove("active");
-  });
-
+  scenes.forEach(scene => scene.classList.remove("active"));
   const target = document.querySelector(id);
-
-  if(target){
-    target.classList.add("active");
-  }
+  if(target) target.classList.add("active");
 }
 
 links.forEach(link=>{
-
   link.addEventListener("click",(e)=>{
-
     e.preventDefault();
-
-    const target = link.getAttribute("href");
-
-    showScene(target);
-
+    showScene(link.getAttribute("href"));
   });
-
 });
 
 showScene("#hero");
